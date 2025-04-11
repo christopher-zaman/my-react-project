d5b46de91f3c3f83b7ef4f808d5c0d1a861e26d0
Christopher Zaman <zamanchristopher@gmail.com>
2025-04-11 12:38:06 -0400

    NavBar

--- src/main.jsx
+++ src/main.jsx
@@ -8,7 +8,7 @@ import './assets/css/main.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-    <BrowserRouter>
+    <BrowserRouter basename="/my-react-project">
         <App />
     </BrowserRouter>
 
